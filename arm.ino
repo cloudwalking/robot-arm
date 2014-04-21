ba2483eca320c5cd8535907beceacf4f134f3160
Reed Morse <reed.morse@gmail.com>
2014-04-20 22:10:36 -0700

    improving

--- arm.ino
+++ arm.ino
@@ -2,9 +2,9 @@
 
 #include <Adafruit_NeoPixel.h>
 
-#define LED_COUNT 10
-#define LED_DATA_PIN 9
-#define LED_DEFAULT_BRIGHTNESS 30
+#define LED_COUNT 3
+#define LED_DATA_PIN 6
+#define LED_DEFAULT_BRIGHTNESS 60
 
 #define RED_COLOR 0
 #define ORANGE_COLOR 32
@@ -14,6 +14,9 @@
 #define BLUE_COLOR 256
 #define PURPLE_COLOR 329
 #define WHITE_COLOR -1
+
+// Chance for white noise, 1/X
+#define WHITE_NOISE_FREQUENCY 7
 
 Adafruit_NeoPixel _pixels = Adafruit_NeoPixel(LED_COUNT, LED_DATA_PIN, NEO_GRB + NEO_KHZ400);
 
@@ -26,53 +29,55 @@ void setup() {
 
 void loop() {
   //rainbowCycle(20);
+  for (int i = 0; i < 15; i++) {
+    animate_backgroundColorWithColorFillingInForward(PURPLE_COLOR, GREEN_COLOR, 40);
+    animate_backgroundColorWithColorFillingInForward(GREEN_COLOR, PURPLE_COLOR, 40);
+  }
   
-  animate_backgroundColorWithColorFillingInForward(TEAL_COLOR, YELLOW_COLOR, 50);
-  animate_backgroundColorWithColorFillingInBackward(YELLOW_COLOR, TEAL_COLOR, 50);
+  for (int i = 0; i < 15; i++) {
+    // animate_backgroundColorWithColorFillingInForward(BLUE_COLOR, YELLOW_COLOR, 50);
+    // animate_backgroundColorWithColorFillingInBackward(YELLOW_COLOR, BLUE_COLOR, 50);
+  }
+  
+  for (int i = 0; i < 15; i++) {
+    // animate_backgroundColorWithColorFillingInForward(RED_COLOR, YELLOW_COLOR, 50);
+    // animate_backgroundColorWithColorFillingInBackward(YELLOW_COLOR, RED_COLOR, 50);
+  }
 }
 
 void animate_backgroundColorWithColorFillingInForward(uint16_t background, uint16_t fill, uint16_t wait) {
   full_color(background);
-  _pixels.show();
+  show();
   delay(wait);
 
   for (int16_t i = 0; i < LED_COUNT; i++) {
     _pixels.setPixelColor(i, color(fill));
     _pixels.show();
-    whiteNoise(10, 1);
+    whiteNoise(WHITE_NOISE_FREQUENCY, (rand() % 3) + 1);
     delay(wait);
   }
 }
 
 void animate_backgroundColorWithColorFillingInBackward(uint16_t background, uint16_t fill, uint16_t wait) {
   full_color(background);
-  _pixels.show();
+  show();
   delay(wait);
 
   for (int16_t i = LED_COUNT - 1; i >= 0; i--) {
     _pixels.setPixelColor(i, color(fill));
     _pixels.show();
-    whiteNoise(10, 1);
+    whiteNoise(WHITE_NOISE_FREQUENCY, (rand() % 3) + 1);
     delay(wait);
   }
 }
 
 void whiteNoise(uint16_t chance, uint16_t pixels) {
-  srand(millis());
+  // srand(millis());
   if (rand() % chance == 0) {
     for (uint16_t i = 0; i < pixels; i++) {
       uint16_t pixel = rand() % LED_COUNT;
       _pixels.setPixelColor(pixel, color(WHITE_COLOR));
     }
-  }
-}
-
-void animate_blueWithYellow(uint16_t wait) {
-  for (uint16_t i = 0; i < LED_COUNT; i++) {
-    full_color(BLUE_COLOR);
-    _pixels.setPixelColor(i, color(YELLOW_COLOR));
-    _pixels.show();
-    delay(wait);
   }
 }
 
@@ -91,6 +96,12 @@ void rainbowCycle(uint8_t wait) {
 void full_color(uint16_t c) {
   for (uint16_t i = 0; i < LED_COUNT; i++) {
     _pixels.setPixelColor(i, color(c));
+  }
+}
+
+void full_off() {
+  for (uint16_t i = 0; i < LED_COUNT; i++) {
+    _pixels.setPixelColor(i, 0);
   }
 }
 
